9792bfea8a6f92f3ba4a3c348e0b2788b4b4a07b
Hugo Junior <hjunioreng@gmail.com>
2025-10-04 11:49:19 -0300

    fix: adiciona prisma generate no prestart e scripts de seed

--- src/index.js
+++ src/index.js
@@ -7,8 +7,13 @@ import router from "./e.routes/index.js"
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
+// app.use(cors());
+app.use(cors({
+  origin: 'https://obra-sync-front.onrender.com', // seu frontend
+  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+  credentials: true
+}));
 app.use(router)
 const PORT = process.env.PORT || 4000;
 
