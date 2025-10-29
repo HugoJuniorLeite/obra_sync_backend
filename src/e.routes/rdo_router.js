45bfac1e6646e1661379c8637a0fdd09528e5c1a
Hugo Junior <hjunioreng@gmail.com>
2025-10-28 23:43:52 -0300

    Corrige endpoint create-rdo e ajusta retorno de erro

--- src/e.routes/rdo_router.js
+++ src/e.routes/rdo_router.js
@@ -1,46 +1,59 @@
-// import api from "./api";
+// // import api from "./api";
 
 
-async function postRdo(payload) {
-  const response = await api.post('/create-rdo', payload);
-  return response.data;
-}
+// async function postRdo(payload) {
+//   const response = await api.post('/create-rdo', payload);
+//   return response.data;
+// }
 
-async function getOccupation() {
-  const response = await api.get('/all-occupations');
-  return response.data;
-}
+// async function getOccupation() {
+//   const response = await api.get('/all-occupations');
+//   return response.data;
+// }
 
-async function getOccupationById(payload) {
-  const response = await api.get(`/occupation-by-id/${payload}`);
-  return response.data;
-}
-
-
-
-async function getPdf(id) {
-  console.log(id)
-  const response = await api.get(`/rdo-by-bill/${id}/pdf`, {
-    responseType: 'blob' // <-- importante
-  });
-  return response.data; // aqui já é Blob
-}
+// async function getOccupationById(payload) {
+//   const response = await api.get(`/occupation-by-id/${payload}`);
+//   return response.data;
+// }
 
 
 
+// async function getPdf(id) {
+//   console.log(id)
+//   const response = await api.get(`/rdo-by-bill/${id}/pdf`, {
+//     responseType: 'blob' // <-- importante
+//   });
+//   return response.data; // aqui já é Blob
+// }
 
-const createRdo = { postRdo, getOccupation, getOccupationById, getPdf }
 
-export default createRdo;
 
-// import rdo_controller, { upload_photos_middleware } from "../a.controllers/rdo_controller.js"
-// import { Router } from "express";
 
-// const rdo_router = Router();
+// const createRdo = { postRdo, getOccupation, getOccupationById, getPdf }
 
-// rdo_router.post("/create-rdo", upload_photos_middleware, rdo_controller.create_rdo_controller);
-// rdo_router.put("/not-executed/:bill_id", rdo_controller.rdo_not_executed);
-// rdo_router.get("/rdo-by-bill/:bill_id", rdo_controller.get_rdo_by_bill_id_controller)
-// rdo_router.get("/rdo-by-project/:project_id", rdo_controller.get_rdo_by_project_controller);
+// export default createRdo;
 
-// export default rdo_router;
+// // import rdo_controller, { upload_photos_middleware } from "../a.controllers/rdo_controller.js"
+// // import { Router } from "express";
+
+// // const rdo_router = Router();
+
+// // rdo_router.post("/create-rdo", upload_photos_middleware, rdo_controller.create_rdo_controller);
+// // rdo_router.put("/not-executed/:bill_id", rdo_controller.rdo_not_executed);
+// // rdo_router.get("/rdo-by-bill/:bill_id", rdo_controller.get_rdo_by_bill_id_controller)
+// // rdo_router.get("/rdo-by-project/:project_id", rdo_controller.get_rdo_by_project_controller);
+
+// // export default rdo_router;
+
+
+import { Router } from "express";
+import rdo_controller, { upload_photos_middleware } from "../a.controllers/rdo_controller.js";
+
+const rdo_router = Router();
+
+rdo_router.post("/create-rdo", upload_photos_middleware, rdo_controller.create_rdo_controller);
+rdo_router.put("/not-executed/:bill_id", rdo_controller.rdo_not_executed);
+rdo_router.get("/rdo-by-bill/:bill_id", rdo_controller.get_rdo_by_bill_id_controller);
+rdo_router.get("/rdo-by-project/:project_id", rdo_controller.get_rdo_by_project_controller);
+
+export default rdo_router;
