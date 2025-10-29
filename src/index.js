906a01e5289a18dcd2e68b975fbc6dded0346e9f
Hugo Junior <hjunioreng@gmail.com>
2025-10-28 21:39:22 -0300

    Corrige endpoint create-rdo e ajusta retorno de erro

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ import router from "./e.routes/index.js"
 const app = express();
 
 app.use(express.json());
-app.use(cors());
-// app.use(cors({
-//   origin: 'https://obra-sync-front.onrender.com', // seu frontend
-//   methods: ['GET','POST','PUT','DELETE','OPTIONS'],
-//   credentials: true
-// }));
+// app.use(cors());
+app.use(cors({
+  origin: 'https://obra-sync-front.onrender.com', // seu frontend
+  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+  credentials: true
+}));
 app.use(router)
 const PORT = process.env.PORT || 4000;
 
