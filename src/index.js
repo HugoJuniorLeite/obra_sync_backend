842078f8facfe949ec95719914c0cc050bfff42f
Hugo Junior <hjunioreng@gmail.com>
2025-10-28 23:13:32 -0300

    Corrige endpoint create-rdo e ajusta retorno de erro

--- src/index.js
+++ src/index.js
@@ -22,3 +22,36 @@ app.listen(PORT, () => {
 });
 
 export default app;
+
+// import dotenv from "dotenv";
+// dotenv.config();
+// import express from "express";
+// import cors from "cors";
+// import router from "./e.routes/index.js"
+// import path from "path"; 
+
+// // Importa as rotas novas
+// import uploadRoutes from "./e.routes/uploadRoutes.js";
+// import pdfRoutes from "./e.routes/pdfRoutes.js";
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+// app.use(router)
+
+// // Servir arquivos estáticos da pasta uploads
+// app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
+
+// // Rotas novas
+// app.use("/api/upload", uploadRoutes);
+// app.use("/api/pdf", pdfRoutes);
+
+
+// const PORT = process.env.PORT || 4000;
+
+// app.listen(PORT, () => {
+//   console.log(`Running on port: ${PORT}`);
+// });
+
+// export default app;
