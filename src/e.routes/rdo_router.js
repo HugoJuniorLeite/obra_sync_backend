6a8b6c24e2f0c3d4fe30881d330cb6204d00cd0a
Hugo Junior <hjunioreng@gmail.com>
2025-10-28 22:09:51 -0300

    deploy

--- src/e.routes/rdo_router.js
+++ src/e.routes/rdo_router.js
@@ -1,42 +1,36 @@
-import { Router } from "express";
-import multer from "multer";
-// import rdo_controller from "../a.controllers/rdo_controller.js";
-import path from "path";
-import rdo_controller, { upload_photos_middleware } from "../a.controllers/rdo_controller.js";
+import api from "./api";
 
-// Configuração do multer
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/'); // pasta onde os arquivos serão salvos
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname);
-  }
-});
 
-const upload = multer({ storage });
+async function postRdo(payload) {
+  const response = await api.post('/create-rdo', payload);
+  return response.data;
+}
 
-const rdo_router = Router();
+async function getOccupation() {
+  const response = await api.get('/all-occupations');
+  return response.data;
+}
 
-rdo_router.post("/create-rdo",
-  (req, res, next) => {
-    // console.log("✅ ROTA /create-rdo FOI CHAMADA!");
-    // console.log("📌 Método:", req.method);
-    // console.log("🌐 URL:", req.url);
-    // console.log("📨 Content-Type:", req.headers["content-type"]);
-    next();
-  },
-  upload_photos_middleware,
-  (req, res, next) => {
-    // console.log("📸 Após multer, req.files:", req.files);
-    next();
-  },
-  rdo_controller.create_rdo_controller
-);
+async function getOccupationById(payload) {
+  const response = await api.get(`/occupation-by-id/${payload}`);
+  return response.data;
+}
 
-// Outras rotas
-rdo_router.put("/not-executed/:bill_id", rdo_controller.rdo_not_executed);
-rdo_router.get("/rdo-by-bill/:bill_id/pdf", rdo_controller.generate_rdo_pdf_controller);
-rdo_router.get("/rdo-by-project/:project_id", rdo_controller.get_rdo_by_project_controller);
 
-export default rdo_router;
+
+async function getPdf(id) {
+  console.log(id)
+  const response = await api.get(`/rdo-by-bill/${id}/pdf`, {
+    responseType: 'blob' // <-- importante
+  });
+  return response.data; // aqui já é Blob
+}
+
+
+
+
+const createRdo = { postRdo, getOccupation, getOccupationById, getPdf }
+
+export default createRdo;
+
+
